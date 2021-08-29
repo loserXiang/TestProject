d28d12eb4897e248de06917779f309fd9264c0cd
loserxiang <loserxiang@163.com>
2021-08-29 11:19:22 +0800

    update .gitignore

---
+++ src/main/java/com/loserxiang/TestProjectApplication.java
@@ -0,0 +1,16 @@
+package com.loserxiang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author loserxiang
+ * @Date 2021/8/29 10:59
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class TestProjectApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TestProjectApplication.class,args);
+    }
+}
