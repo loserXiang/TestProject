05e1acad5d2486c028aa6dfe39fd1b95d34ca9ed
loserxiang <loserxiang@163.com>
2021-08-29 11:53:01 +0800

    设置测试控制器

--- src/main/java/com/loserxiang/controller/TestController.java
+++ src/main/java/com/loserxiang/controller/TestController.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
+ * 测试控制器
+ *
  * @Author loserxiang
  * @Date 2021/8/29 11:02
  * @Version 1.0
